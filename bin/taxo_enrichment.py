f07ea3f83f4cc54938d89533e1393500b2cf5f9e
Vincent Darbot <vincent.darbot@inrae.fr>
2021-06-06 01:24:28 +0200

    bin/taxo_enrichment.py : add significance

--- bin/taxo_enrichment.py
+++ bin/taxo_enrichment.py
@@ -74,51 +74,53 @@ def process_stats(nb_total, nb_over, nb_under, rank_to_observ_counts, pvalue, pa
 	cur = 0
 
 	for rank, categorie in rank_to_observ_counts.items():
-		print(rank)
-		print()
-		print()
-		print()
 		categorie['all'] = dict(categorie['all'] )
 		categorie['under'] = dict(categorie['under'] )
 		categorie['over'] = dict(categorie['over'] )
 
-		for observ, count in categorie['over'].items():
+		
+		for sens in ["under","over"]:
+			if sens == "under":
+				nb_sens = nb_under
+			else:
+				nb_sens = nb_over
+			for observ, count in categorie[sens].items():
 
-			ratio, p = stats.fisher_exact([[nb_total, categorie['all'][observ]], [nb_over, categorie['over'][observ]]])
-			if p < pvalue:
-				print('OVER')
-				print(rank, observ)
-				print(nb_total, categorie['all'][observ], nb_over, categorie['over'][observ])
-				print(ratio,p)
-				sign_results[cur] = [rank, "OVER", observ , nb_total, categorie['all'][observ], nb_over, categorie['over'][observ], ratio, p]
-				print()
-				to_adj.append(p)
-				cur+=1
-		for observ, count in categorie['under'].items():
-			ratio, p = stats.fisher_exact([[nb_total, categorie['all'][observ]], [nb_under, categorie['under'][observ]]])
-			if p < pvalue:
-				print('UNDER')
-				print(rank, observ)
-				print(nb_total, categorie['all'][observ], nb_under, categorie['under'][observ])
-				print(ratio,p)
-				sign_results[cur] = [rank, "UNDER", observ , nb_total, categorie['all'][observ], nb_under, categorie['under'][observ], ratio, p]
-				print()
-				to_adj.append(p)
-				cur+=1
+				ratio, p = stats.fisher_exact([[nb_total, categorie['all'][observ]], [nb_sens, categorie[sens][observ]]])
+				if p < pvalue:
+
+					sign_results[cur] = [rank, sens, observ , nb_total, categorie['all'][observ], nb_sens, categorie[sens][observ], ratio, p]
+					to_adj.append(p)
+					cur+=1
+
 
 	p_adjusted = multipletests(to_adj, method='bonferroni', alpha=pvalue)
 
 	for j in range(len(list(p_adjusted[1]))):
 
 		if padj_filter:
-			if list(p_adjusted[1])[j] < pvalue:
-				sign_results[j].append(list(p_adjusted[1])[j])
+			padj = list(p_adjusted[1])[j]
+			if padj < pvalue:
+				sign_results[j].append(padj)
 
 			else:
 				del sign_results[j]
-
+				continue
 		else:
-			sign_results[j].append(list(p_adjusted[1])[j])
+			sign_results[j].append(padj)
+
+		if padj < 0.001 :
+			sign = "***"
+		elif 0.001 < padj < 0.01:
+			sign = "**"
+		elif 0.01 < padj < 0.05:
+			sign = "*"
+		elif 0.05 < padj < 0.1:
+			sign = "."
+		else:
+			sign = " "
+		
+		sign_results[j].append(sign)
 	
 	return sign_results
 
@@ -126,7 +128,7 @@ def write_output(sign_results, output):
 
 	FH_out = open(output, 'wt')
 
-	FH_out.write('Taxonomic_rank\tDESeq2_Log2fc_sens\tTaxonomic_name\tNb_total_clusters\tNb_observ_taxonomic_name_from_total\tNb_Deseq2_differentially_abundant(OVER_or_UNDER)\tNb_observ_taxonomic_names_from_diff_abundant\tRatio_between_total_and_diff_abundant\tpvalue\tpadj\n')
+	FH_out.write('Taxonomic_rank\tDESeq2_Log2fc_sens\tTaxonomic_name\tNb_total_clusters\tNb_observ_taxonomic_name_from_total\tNb_Deseq2_differentially_abundant(OVER_or_UNDER)\tNb_observ_taxonomic_names_from_diff_abundant\tRatio_between_total_and_diff_abundant\tPvalue\tPadj\tSignificance\n')
 
 	for id, signs in sign_results.items():
 		FH_out.write("\t".join(map(str,signs))+"\n")
@@ -142,7 +144,7 @@ if __name__ == "__main__":
 	# Inputs
 	group_input = parser.add_argument_group('Inputs')
 	group_input.add_argument('-t', '--table', required=True, help='Table output of DESeq2 (!! padj cutoff must be set to 1.')
-	group_input.add_argument('-r', '--ranks', help='Taxonomy rank of interest between : "Kingdom",Phylum", "Class", "Order", "Family", "Genus", "Species". To choose differents ranks, separate them by commas (ie kingdom)')
+	group_input.add_argument('-r', '--ranks', default='Kingdom,Phylum,Class,Order,Family,Genus', help='Taxonomy rank of interest between : "Kingdom",Phylum", "Class", "Order", "Family", "Genus", "Species". To choose differents ranks, separate them by commas (ie kingdom)')
 	group_input.add_argument('--pvalue_deseq', default=0.05, type=float, help='pvalue cutoff from DESeq2 to considere a cluster to be differentially significant. [Default: %(default)s]')
 	group_input.add_argument('-p','--pvalue', default=0.05, type=float, help='pvalue to considere a taxonomie observation to be differentially significant. [Default: %(default)s]')
 	group_input.add_argument('--padj_filter', default=False, action='store_true', help='if flag added, only observations significant after bonferroni correction will be written [Default: False]')
